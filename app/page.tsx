b58abd65149b78dc853eb414a8248452a036e439
Sudo-Self <sudo-self@proton.me>
2024-03-08 23:38:25 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default async function Page() {
   return (
     <div className="note--empty-state">
       <span className="note-text--empty-state">
-        Click a note on the left to view something! 🥺
+        Click a note to read. Login to post a note!
       </span>
     </div>
   )
